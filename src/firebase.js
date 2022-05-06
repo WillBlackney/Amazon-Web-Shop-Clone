19210eb01415bc5bff360bd47ad60701d072fe87
William Blackney <william.blackney@hotmail.com>
2022-05-06 13:11:51 +0200

    Header top bar push

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRA1SNZIjKW4BU-5yAupe3MwSyPYQMgKk",
+    authDomain: "challenge-91043.firebaseapp.com",
+    projectId: "challenge-91043",
+    storageBucket: "challenge-91043.appspot.com",
+    messagingSenderId: "772274687710",
+    appId: "1:772274687710:web:7d26c9ba072427d8e53c4f",
+    measurementId: "G-TDZ19R5HCY"
+  };
